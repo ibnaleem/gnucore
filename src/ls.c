24e7b7c7ddd15f03210a2305b9807c96f235ee54
ibnaleem <ibnaleem@outlook.com>
2026-03-21 23:15:44 +0000

    chore: move ls.c to src/ directory

---
+++ src/ls.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+bool is_directory(char *path) {
+
+  struct stat st;
+
+  if (stat(path, &st) != 0 ) {return false;}
+
+  return S_ISDIR(st.st_mode);
+}
+
+int print_directory_contents(char *path) {
+    
+  DIR *curr_directory_pointer = opendir(path);
+
+  if (curr_directory_pointer == NULL) {
+    printf("ls: cannot access '%s': permission denied\n", path);
+    return 1;
+  }
+  
+  struct dirent *entry = readdir(curr_directory_pointer);
+
+  while (entry != NULL) {
+
+    if (is_directory(entry->d_name)) {
+      printf("%s/\n", entry->d_name);
+    } else {
+      printf("%s\n", entry->d_name);
+    }
+    entry = readdir(curr_directory_pointer);
+  }
+
+  closedir(curr_directory_pointer);
+
+  return 0;
+}
+
+int main(int argc, char *argv[]) {
+
+  if (argc == 1) {
+
+    print_directory_contents(".");
+
+  } else if (argc == 2) {
+    char *path = argv[1];
+    print_directory_contents(path);
+  } else {
+
+    for (int i = 1; i < argc; i++) {
+      char *path = argv[i];
+
+      if (!is_directory(path)) {
+        printf("ls: cannot access '%s': not a directory\n", path);
+      } else {
+        printf("\n");
+        printf("%s:\n", path);
+        print_directory_contents(path);
+      }
+    }
+
+  }
+
+  return 0;
+
+}
