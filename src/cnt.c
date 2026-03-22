6e8da0150de08821629d7c21c6dab67d4a8682dd
ibnaleem <ibnaleem@outlook.com>
2026-03-22 12:52:33 +0000

    feat: create count_lines_in_file() function

--- src/cnt.c
+++ src/cnt.c
@@ -12,3 +12,24 @@ bool is_directory(char *path) {
   return S_ISDIR(st.st_mode);
 }
 
+int count_lines_in_file(char *path) {
+  FILE* fptr = fopen(path, "r");
+
+  if (fptr == NULL) {
+    printf("cnt: unable to open '%s': permission denied\n", path);
+    return 1;
+  }
+
+  char buff[4096];
+  int n = sizeof(buff);
+
+  int lines = 0;
+
+  while (fgets(buff, n, fptr) != NULL) {
+    lines++;
+  }
+
+  fclose(fptr);
+  
+  return lines;
+}
